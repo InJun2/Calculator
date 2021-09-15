ce752ab584a2af5a072d9d1a783b958b8444078d
InJun2 <50690859+InJun2@users.noreply.github.com>
2021-09-15 20:51:07 +0900

    계산 방법 수정
     각 연산자 문자열 일치마다 for문 실행해 3번 실행했던 것을 hashMap을 이용해 문자열을 확인하고 for문 한번 실행하여 계산
    코드 간결화 및 재 수정 예정

--- Calculator2/src/Calculate/Calculate.java
+++ Calculator2/src/Calculate/Calculate.java
@@ -12,46 +12,50 @@ public class Calculate {
 	private final String PLUS = "+";
 	private final String MINUS = "-";
 	
-	
 	protected Calculate(Numbers numbers, Operators opers) {
 		this.opers = opers.getOperators();
 		this.numbers = numbers.getNumbers();
-		multiplication();
-		addition();
-		subtraction();
-		Output.print("°è»êµÈ °á°ú´Â "+this.numbers.get(0) + "ÀÔ´Ï´Ù");
+		calculate();
+		Output.print("ê³„ì‚°ëœ ê²°ê³¼ëŠ” "+this.numbers.get(this.numbers.size()-1) + "ì…ë‹ˆë‹¤");
 		
 	}
 	
+	private void calculate() {
+		int result = numbers.get(0);
+		for(int i=0; i<opers.size();i++) {
+			result = Oper.calculate(opers.get(i),result, numbers.get(i+1));
+			numbers.set(i+1, result);
+		}
+	}
 	
-	private void multiplication() {
-		for (int i = 0; i < opers.size(); i++) {
-			if (MULTIPLY.equals(opers.get(i))) {
-				numbers.set(i, numbers.get(i) * numbers.get(i + 1));
-				numbers.remove(i + 1);
-				opers.remove(i);
-			}
-		}
-
-	}
-
-	private void addition() {
-		for (int i = 0; i < opers.size(); i++) {
-			if (PLUS.equals(opers.get(i))) {
-				numbers.set(i, numbers.get(i) + numbers.get(i + 1));
-				numbers.remove(i + 1);
-				opers.remove(i);
-			}
-		}
-	}
-
-	private void subtraction() {
-		for (int i = 0; i < opers.size(); i++) {
-			if (MINUS.equals(opers.get(i))) {
-				numbers.set(i, numbers.get(i) - numbers.get(i + 1));
-				numbers.remove(i + 1);
-				opers.remove(i);
-			}
-		}
-	}
+//	private void multiplication() {
+//		for (int i = 0; i < opers.size(); i++) {
+//			if (MULTIPLY.equals(opers.get(i))) {
+//				numbers.set(i, numbers.get(i) * numbers.get(i + 1));
+//				numbers.remove(i + 1);
+//				opers.remove(i);
+//			}
+//		}
+//
+//	}
+//
+//	private void addition() {
+//		for (int i = 0; i < opers.size(); i++) {
+//			if (PLUS.equals(opers.get(i))) {
+//				numbers.set(i, numbers.get(i) + numbers.get(i + 1));
+//				numbers.remove(i + 1);
+//				opers.remove(i);
+//			}
+//		}
+//	}
+//
+//	private void subtraction() {
+//		for (int i = 0; i < opers.size(); i++) {
+//			if (MINUS.equals(opers.get(i))) {
+//				numbers.set(i, numbers.get(i) - numbers.get(i + 1));
+//				numbers.remove(i + 1);
+//				opers.remove(i);
+//			}
+//		}
+//	}
 }
